c3b9754387fd5fd3378c6e1e48bfd6e82c1f4d8d
DiversityIsland <osh.gaziz@gmail.com>
2021-11-06 15:40:47 +0300

    resolve1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld message1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(message1.getMessage());
+        HelloWorld message2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(message2.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getName());
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getName());
+
+        System.out.println(message1 == message2);
+        System.out.println(cat1 == cat2);
     }
 }
